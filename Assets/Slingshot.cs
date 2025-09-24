aa25ea018eae25c382bee72c74fc0255ab0701c3
ameermizher-sketch <ameer.mizher@gmail.com>
2025-09-24 00:36:57 -0400

    Update Slingshot.cs

--- Assets/Slingshot.cs
+++ Assets/Slingshot.cs
@@ -11,7 +11,15 @@
      void OnMouseExit() {
          print( "Slingshot:OnMouseExit()" );
      }
+ void OnMouseEnter() {
+    //print("Slingshot:OnMouseEnter()");
+    launchPoint.SetActive( true );
+}
  
+void OnMouseExit() {
+    //print("Slingshot:OnMouseExit()");
+    launchPoint.SetActive( false );
+}
      // void Start() {…}  // Please delete the unused Start() and Update() methods
      // void Update() {…}
      
@@ -34,4 +42,36 @@
          launchPoint.SetActive( false );                                // b
      }
  }
+      // fields set in the Unity Inspector pane 
+     [Header("Inscribed")]                                                       // a
+     public GameObject        projectilePrefab;
  
+     // fields set dynamically
+     [Header("Dynamic")]                                                         // a
+     public GameObject        launchPoint;
+     public Vector3           launchPos;                                         // b
+     public GameObject        projectile;                                        // b
+     public bool              aimingMode;                                        // b
+ 
+     void Awake() {
+         Transform launchPointTrans = transform.Find("LaunchPoint");
+         launchPoint = launchPointTrans.gameObject;
+         launchPoint.SetActive( false );
+         launchPos = launchPointTrans.position;                                 // c
+     }
+         void OnMouseEnter() { … }  // Do not change OnMouseEnter()
+ 
+     void OnMouseExit() { … }   // Do not change OnMouseExit()
+     
+     void OnMouseDown() {                                                       // d
+         // The player has pressed the mouse button while over Slingshot
+         aimingMode = true;
+         // Instantiate a Projectile
+         projectile = Instantiate( projectilePrefab ) as GameObject;
+         // Start it at the launchPoint
+         projectile.transform.position = launchPos;
+         // Set it to isKinematic for now
+         projectile.GetComponent<Rigidbody>().isKinematic = true;               // e
+     }
+ 
+}
